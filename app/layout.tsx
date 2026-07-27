c13530f7d647235e05c5e59d6c918d550201b149
simarjeetswork <simarjeet.s.work@gmail.com>
2026-07-27 12:17:26 +0530

    fixed chart section

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import SmoothScrollProvider from "@/components/SmoothScrollProvider";
-import PricingModel from "@/components/layout/PricingModel";
 
 const geist = Geist({
   variable: "--font-geist",
--- components/sections/factCentre/Analysis.tsx
+++ components/sections/factCentre/Analysis.tsx
@@ -29,20 +29,22 @@ export default function Analysis() {
 
         }, { scope: container, dependencies: [] }
     );
-    
+
     return (
         <>
             <section className="fct_glc pt-20 pb-20 px-6 relative overflow-hidden bg-white" ref={container}>
                 <div className="container mx-auto ">
+                    <div className=" w-full lg:w-[90%] mx-auto">
                     <div className="relative z-1 text-center">
                         <AnimatedText as="p" className="text-blue text-center text-xs sm:text-sm uppercase font-mono font-normal bg-chip inline-block px-4 rounded-2xl py-1 mb-5">Rejection Analysis</AnimatedText>
-                        <Heading as="h2" className=" text-2xl sm:text-4xl xl:text-5xl leading-[110%] text-center font-light font-primary text-heading mb-12">Why transactions fail — so you<br className="hidden sm:block"/> can fix them at the source.</Heading>
+                        <Heading as="h2" className=" text-2xl sm:text-4xl xl:text-5xl leading-[110%] text-center font-light font-primary text-heading mb-12">Why transactions fail — so you<br className="hidden sm:block" /> can fix them at the source.</Heading>
                         <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
                         </div>
                     </div>
-     <Chart/>
+                    <Chart />
+                    </div>
                 </div>
-           
+
             </section>
         </>
     )
