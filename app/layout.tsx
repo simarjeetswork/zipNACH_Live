4714bd834e5b4be0eabf9cca62ff429765b518f7
simarjeetswork <simarjeet.s.work@gmail.com>
2026-07-31 14:46:58 +0530

    lenis fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       <body>
 
         <SmoothScrollProvider>
-          <ScrollTriggerRefresh />
+          {/* <ScrollTriggerRefresh /> */}
           <Header />
           {children}
 
